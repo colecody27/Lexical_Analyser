eca92773e426d38cedf40db633ae6824ecf841c7
Cody Cole <71093271+colecody27@users.noreply.github.com>
2022-11-07 17:28:53 -0800

    Update on main with printFunction
    Contains the printRecord function which receives the output from the lexer function and outputs to "output_file.txt". 
    *Requires additional formatting to align on the output file.*

--- Lexical_Analyser/Lexer/main.java
+++ Lexical_Analyser/Lexer/main.java
@@ -9,39 +9,98 @@ public class main {
 	// Driver program to use lexer class
 	public static void main(String[] args) {
 
-		HashSet<Character> seperators = new HashSet<Character>();
-		seperators.add('(');
-		seperators.add(')');
-		seperators.add(';');
+		// HashSet<Character> seperators = new HashSet<Character>();
+		// seperators.add('(');
+		// seperators.add(')');
+		// seperators.add(';');
+		//
+		// HashSet<Character> operators = new HashSet<Character>();
+		// operators.add('=');
+		// operators.add('<');
+		//
+		// HashSet<String> keywords = new HashSet<String>();
+		// keywords.add("while");
+		// keywords.add("for");
 
-		HashSet<Character> operators = new HashSet<Character>();
-		operators.add('=');
-		operators.add('<');
+		// TEST PRINTRECORD FUNCTION
+		ArrayList<String[]> record = new ArrayList<String[]>();
+		String[] line1 = new String[] { "keyword", "while" };
+		String[] line2 = new String[] { "separator", "(" };
+		String[] line3 = new String[] { "identifier", "s" };
+		String[] line4 = new String[] { "operator", "<" };
+		String[] line5 = new String[] { "identifier", "upper" };
+		String[] line6 = new String[] { "separator", ")" };
+		String[] line7 = new String[] { "identifier", "t" };
+		String[] line8 = new String[] { "operator", "=" };
+		String[] line9 = new String[] { "real", "33.00" };
+		String[] line10 = new String[] { "separator", ";" };
 
-		HashSet<String> keywords = new HashSet<String>();
-		keywords.add("while");
-		keywords.add("for");
+		record.add(line1);
+		record.add(line2);
+		record.add(line3);
+		record.add(line4);
+		record.add(line5);
+		record.add(line6);
+		record.add(line7);
+		record.add(line8);
+		record.add(line9);
+		record.add(line10);
 
-		// Test splitString function
-		String input_spaces = "while  (s < upper)   t = 33.00;";
-		String input_nospaces = "while(s<upper)t=33.00;";
-		ArrayList<String> output = splitString(input_spaces, seperators, operators);
+		printRecord(record);
 
-		for (String word : output) {
-			System.out.println(word);
-		}
+		// TEST SPLITSTRING FUNCTION
+		// String input_spaces = "while (s < upper) t = 33.00;";
+		// String input_nospaces = "while(s<upper)t=33.00;";
+		// ArrayList<String> output = splitString(input_spaces, seperators, operators);
+		//
+		// for (String word : output) {
+		// System.out.println(word);
+		// }
 
 		// Create file and scanner instances
-		File file = new File("input_scode.txt");
-		Scanner scanFile = null;
+		// File file = new File("input_scode.txt");
+		// Scanner scanFile = null;
+		//
+		// // Try to read file
+		// try {
+		// scanFile = new Scanner(file);
+		// } catch (FileNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+	}
 
-		// Try to read file
+
+	public static void printRecord(ArrayList<String[]> record) {
+		File output_file = null;
+		FileWriter writer = null;
+
 		try {
-			scanFile = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			output_file = new File("output_file.txt");
+
+			// If file already exists, delete it.
+			if (!output_file.createNewFile())
+				output_file.delete();
+
+			writer = new FileWriter("output_file.txt");
+
+			// Ouput headline to file
+			writer.write(String.format("%-20s %s", "Token", "Lexeme\n"));
+			writer.write("__________________________________\n");
+
+			// Ouput each record to file
+			for (String[] arr : record) {
+				String padding = "                    ";
+				padding = padding.substring(0, padding.length() - arr[0].length());
+				writer.write(String.format("%s%s%-20s\n", arr[0], padding, arr[1]));
+			}
+			writer.close();
+
+		} catch (IOException e) {
+			System.out.println("Creating new file or writer failed");
 			e.printStackTrace();
 		}
+		System.out.println("Done");
 	}
 
 
